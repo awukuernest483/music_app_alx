eb1a53524e61d740451bd140b904c097b61912f4
Ernest Awuku Junior <awukuernest@gmail.com>
2025-08-29 13:45:40 +0000

    updated auth page design

--- music_app_alx/src/components/button.tsx
+++ music_app_alx/src/components/button.tsx
@@ -1,18 +1,31 @@
 import React from "react";
+import spotifylogo from "../assets/spotifylogo.svg";
 
 interface ButtonProps {
   onClick: () => void;
   text: string;
   type?: "button" | "submit" | "reset";
+  color?: string;
+  textcolor?: string;
+  imgpath?: string;
 }
 
-const Button = ({ onClick, text, type = "button" }: ButtonProps) => {
+const Button = ({
+  onClick,
+  text,
+  type = "button",
+  color,
+  textcolor,
+  imgpath,
+}: ButtonProps) => {
   return (
     <button
       type={type}
       onClick={onClick}
-      className="bg-[#0DBDF2] text-[#000000] p-4 rounded-md cursor-pointer hover:opacity-90 transition"
+      className={`${color ?? "bg-[#0DBDF2]"} ${textcolor ?? "text-[#000000]"}
+      p-4 rounded-lg cursor-pointer gap-4 text-center text-xl justify-center w-1/2 items-center hover:opacity-90 transition flex`}
     >
+      {imgpath && <img src={spotifylogo} className="w-10 h-10" />}
       {text}
     </button>
   );
--- music_app_alx/src/routes/authpage.tsx
+++ music_app_alx/src/routes/authpage.tsx
@@ -1,32 +1,48 @@
 import React from "react";
 import CustomInput from "../components/custominput";
 import Button from "../components/button";
-import authimage from "../assets/images/authimage.png";
+import authimage from "../assets/images/authback.jpg";
 import { redirectToAuthCodeFlow, spotifyLogin } from "../script";
+import logo from "../assets/logo.svg";
 
 const AuthPage = () => {
   return (
-    <div className="w-full h-full flex">
+    <div className="w-full h-full flex ">
       <img
         src={authimage}
         alt="Auth"
-        className="w-full h-full object-cover lg:flex hidden"
+        className="w-1/2 h-full object-cover lg:flex hidden"
       />
-      <div className="flex flex-col w-full h-full">
-        <form className="flex flex-col gap-6 m-auto w-3/4 text-[#9CB2BA]">
-          <h1 className="text-2xl font-semibold text-center">Welcome Back</h1>
+      <div className="flex flex-col w-full h-full flex-1">
+        <form className="flex flex-col gap-20 m-auto w-3/4 text-[#9CB2BA]">
+          <div className="items-center justify-center flex-col flex">
+            <img src={logo} className="h-30 mt-20 mb-20" />
+
+            <p className="font-bold text-4xl text-center text-white">
+              Feel the Beat, Live the Moment
+            </p>
+            <p className="text-white/50 text-xl text-center text-wrap">
+              Your music, your vibe. Discover, stream, and groove anywhere.
+            </p>
+          </div>
+
           {/* <CustomInput placeholder="Username or Email" type="text" />
           <CustomInput placeholder="Password" type="password" /> */}
           {/* <p className="text-base text-left cursor-pointer">Forgot Password?</p> */}
-          <Button
-            text="Login with Spotify"
-            onClick={(e) => {
-              e.preventDefault();
-              console.log("Work");
-              spotifyLogin();
-            }}
-            type="submit"
-          />
+          <div className=" w-full items-center flex justify-center">
+            <Button
+              textcolor="text-white"
+              color="bg-[#1DB954]"
+              text="Login with Spotify"
+              onClick={(e) => {
+                e.preventDefault();
+                console.log("Work");
+                spotifyLogin();
+              }}
+              type="submit"
+              imgpath="sksk"
+            />
+          </div>
         </form>
       </div>
     </div>
